111f340b22509f09689be6b8c8c789f203eb3ce8
Jie Kang <jkang@redhat.com>
2019-06-14 11:46:33 -0400

    Initial commit. Add empty View

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jmc-differential-analysis
+
+JMC Plugin to analyze differences between two JFR recordings of the same application. Targeted for analyzing change in performance after modification of configuration for GC, Memory, etc.
---
+++ com.redhat.jkang.flightrecorder.ext.diffanalysis/src/main/java/com/redhat/jkang/flightrecorder/ext/diffanalysis/views/DiffAnalysisView.java
@@ -0,0 +1,20 @@
+package com.redhat.jkang.flightrecorder.ext.diffanalysis.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class DiffAnalysisView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
